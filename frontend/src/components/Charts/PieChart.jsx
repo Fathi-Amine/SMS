9961c04b28c2068e3ca0de85d2cf35b075dcdcd9
Fathi-Amine <aminefathimw@gmail.com>
2024-02-29 19:17:07 +0100

    Boilerplate finished for components and pages

--- frontend/src/components/Charts/PieChart.jsx
+++ frontend/src/components/Charts/PieChart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Button = () => {
+const PieChart = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Button = () => {
     );
 };
 
-export default Button;
+export default PieChart;
