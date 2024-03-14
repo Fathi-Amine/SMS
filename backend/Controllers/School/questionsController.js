f77b9e397c997d50597f14c3bf1a1dbc6e3903cf
Fathi-Amine <112888632+Fathi-Amine@users.noreply.github.com>
2024-03-14 14:33:53 +0000

    SMS-28 finishing the question crud

--- backend/Controllers/School/questionsController.js
+++ backend/Controllers/School/questionsController.js
@@ -11,6 +11,11 @@ exports.createQuestion = AsyncHandler(async (req, res) => {
     if (!examFound) {
         throw new Error("Exam not found");
     }
+
+    const questionExists = await Question.findOne({ question });
+    if (questionExists) {
+        throw new Error("Question already exists");
+    }
     //create exam
     const questionCreated = await Question.create({
         question,
@@ -29,5 +34,45 @@ exports.createQuestion = AsyncHandler(async (req, res) => {
         status: "success",
         message: "Question created",
         data: questionCreated,
+    });
+});
+
+exports.getQuestions = AsyncHandler(async (req, res) => {
+    const questions = await Question.find();
+    res.status(201).json({
+        status: "success",
+        message: "Question fetched successfully",
+        data: questions,
+    });
+});
+
+exports.getQuestion = AsyncHandler(async (req, res) => {
+    const question = await Question.findById(req.params.id);
+    res.status(201).json({
+        status: "success",
+        message: "Question fetched successfully",
+        data: question,
+    });
+});
+
+exports.updateQuestion = AsyncHandler(async (req, res) => {
+    const { question, optionA, optionB, optionC, optionD, correctAnswer } =
+        req.body;
+    const questionFound = await Question.findById(req.params.id);
+    if (!questionFound) {
+        throw new Error("Question not found");
+    }
+    questionFound.question = question;
+    questionFound.optionA = optionA;
+    questionFound.optionB = optionB;
+    questionFound.optionC = optionC;
+    questionFound.optionD = optionD;
+    questionFound.correctAnswer = correctAnswer;
+    questionFound.createdBy = req.user.id;
+    await questionFound.save();
+    res.status(201).json({
+        status: "success",
+        message: "Question updated successfully",
+        data: questionFound,
     });
 });
